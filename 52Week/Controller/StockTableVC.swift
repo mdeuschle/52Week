b71357ff128c636621064ec7d7fd9aa078d55bde
Matt Deuschle <mdeuschle@gmail.com>
2018-03-17 21:28:20 -0700

    custom cell

---
+++ 52Week/Controller/StockTableVC.swift
@@ -0,0 +1,33 @@
+//
+//  StockTableVC.swift
+//  52Week
+//
+//  Created by Matt Deuschle on 3/17/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class StockTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StockCell", for: indexPath) as? StockCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
---
+++ 52Week/View/StockCell.swift
@@ -0,0 +1,18 @@
+//
+//  StockCell.swift
+//  52Week
+//
+//  Created by Matt Deuschle on 3/17/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class StockCell: UITableViewCell {
+
+    @IBOutlet private weak var tickerSymbolLabel: UILabel!
+    @IBOutlet private weak var stockNameLabel: UILabel!
+    @IBOutlet private weak var stockPrice: UILabel!
+    @IBOutlet private weak var stockSlider: UISlider!
+
+}
